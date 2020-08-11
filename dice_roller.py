f3b37b08c48a34086a9a2bf43a2dcd5507ceb9e6
Mbah Stanley <stanmest77@gmail.com>
2020-08-11 15:04:38 +0100

    Added a player name

--- dice_roller.py
+++ dice_roller.py
@@ -1,17 +1,18 @@
 import random
 def main():
+  player = str(input("Enter your name "))
   dice_rolls = 2
   dice_sum = 0
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
     if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
+      print(f'{player} You rolled a {roll}! Critical Fail')
     elif roll == 6:
-      print(f'You rolled a {roll}! Critical Success!')
+      print(f'{player} You rolled a {roll}! Critical Success!')
     else:
-      print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+      print(f'{player} You rolled a {roll}')
+  print(f'{player} You have rolled a total of {dice_sum}')
 if __name__== "__main__":
   main()
         
